b8c80f05af397028b5c8fb1c08508738d5c8c724
Victor Alonso <Vicgital@hotmail.com>
2023-06-12 18:54:25 -0600

    Initial Commit

---
+++ GithubCopilotTest/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var person = new Person
+{
+    FirstName = "John",
+    LastName = "Doe"
+};
+
+Console.WriteLine(person.GetFullName());
+
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+
+    public string GetFullName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+
+    public override string ToString() =>
+     JsonSerializer.Serialize(this);
+
+
+}
